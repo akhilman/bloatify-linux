3a5ad57fdbfc168967b3776b72b20a8a92e8b7d2
Ildar Akhmetgaleev <akhilman@gmail.com>
2024-03-31 16:01:49 +0700

    Add `--global` option to `deno install`

--- bloatify-linux.sh
+++ bloatify-linux.sh
@@ -301,12 +301,13 @@ install_deno_tools() {
 		|| exit $?
 
 	deno install \
-		--force --allow-read --allow-sys=cpus --allow-env \
-		--allow-write=$XDG_CONFIG_DIR/configstore \
+		--global --force \
+		--allow-read --allow-sys=cpus --allow-env --allow-write=$XDG_CONFIG_DIR/configstore \
 		--name=cspell npm:cspell \
 		|| exit $?
 	deno install \
-		--force --allow-read --allow-env --allow-sys=cpus,uid --allow-run \
+		--global --force \
+		--allow-read --allow-env --allow-sys=cpus,uid --allow-run \
 		--name=diagnostic-languageserver npm:diagnostic-languageserver \
 		|| exit $?
 
